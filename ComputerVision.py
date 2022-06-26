af25acd621c066b32ed175bc7a6fd8824846aa08
Your Name <you@example.com>
2022-06-26 17:53:25 +0200

    a

--- ComputerVision.py
+++ ComputerVision.py
@@ -7,7 +7,6 @@ import io
 from Generator import Generator
 from Predict import Predict
 
-import os
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "C:/D/learn/flask/project.json"
 
 class ComputerVision:
